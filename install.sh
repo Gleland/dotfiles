b7a60546cbff8b5a0992f3db2043c89f31d8755e
GThompson <garrett.leland.thompson@gmail.com>
2017-07-25 18:41:22 -0700

    added install file for backing up and replacing old dotfiles

---
+++ install.sh
@@ -0,0 +1,10 @@
+cp ~/.config/nvim/init.vim ~/.config/nvim/init.vim.bak
+cp ~/.bashrc ~/.bashrc.bak
+cp ~/.bash_profile ~/.bash_profile.back
+
+
+
+cp init.vim ~/.config/nvim/init.vim
+cp bashrc ~/.bashrc
+cp bash_profile ~/.bash_profile
+source ~/.bash_profile

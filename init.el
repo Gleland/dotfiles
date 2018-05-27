2175044f27f2a9cf5490cd9ee85ed3579cd77c61
GThompson <garrett.leland.thompson@gmail.com>
2018-05-27 10:42:31 -0700

    org filing system

--- init.el
+++ init.el
@@ -1,55 +1,26 @@
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives 
+             '("melpa" . "http://melpa.org/packages/"))
+;;(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+;;(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 (package-initialize)
 
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
+;; Bootstrap use-package
 (unless (package-installed-p 'use-package)
+  (package-refresh-contents)
   (package-install 'use-package))
-
-(setq use-package-verbose t)
-(setq use-package-always-ensure t)
-
-(require 'use-package)
-
-(use-package auto-compile
-  :config (auto-compile-on-load-mode))
-(setq load-prefer-newer t)
-;;
-;;
-(setq mac-command-modifier 'meta)
-(setq mac-option-modifier 'none)
-
-(add-to-list 'load-path "~/.emacs.d/evil")
-(require 'evil)
-(evil-mode 1)
-
-(use-package org-bullets
-  :init
-  (add-hook 'org-mode-hook #'org-bullets-mode))
-(setq org-ellipsis "⤵")
-;;
-(defun glt-reload-init-file ()
-  (interactive)
-  (load-file "~/.emacs.d/init.el"))
-
-;; global key bindings
-(global-set-key (kbd "C-c C-l") 'glt-reload-init-file)    ; Reload .emacs file
-
-
+(require 'ob-tangle)
+(org-babel-load-file (expand-file-name "~/.emacs.d/glt-init.org"))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (leuven-theme use-package org-bullets auto-compile))))
+ '(package-selected-packages (quote (powerline-evil ace-jump-helm-line))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(load-theme 'leuven t) ;; https://github.com/fniessen/emacs-leuven-theme 

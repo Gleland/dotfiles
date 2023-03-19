40904772a27f3830c1d6d1ab284738ed56561a3e
Garrett Thompson <garrett.leland.thompson@gmail.com>
2023-03-19 10:19:27 -0600

    update vim installation steps

--- install_vim_stuff.sh
+++ install_vim_stuff.sh
@@ -5,7 +5,6 @@ brew install neovim
 #
 # https://github.com/LazyVim/lazyvim
 #
-mkdir -p ~/.config/nvim/
 mv ~/.config/nvim ~/.config/nvim.bak
 
 # optional but recommended
@@ -13,17 +12,16 @@ mv ~/.local/share/nvim ~/.local/share/nvim.bak
 mv ~/.local/state/nvim ~/.local/state/nvim.bak
 mv ~/.cache/nvim ~/.cache/nvim.bakmv ~/.config/nvim ~/.config/nvim.bak
 
-# optional but recommended
-mv ~/.local/share/nvim ~/.local/share/nvim.bak
-mv ~/.local/state/nvim ~/.local/state/nvim.bak
-mv ~/.cache/nvim ~/.cache/nvim.bak
-
 # clone latest version of stater
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 # remove the git repo after cloning, we won't need it after
 rm -rf ~/.config/nvim/.git
 
-# symlink config files
+# remove empty templates from LazyVim and symlink local config files
+rm ~/.config/nvim/lua/config/autocmds.lua
+rm ~/.config/nvim/lua/config/keymaps.lua
+rm ~/.config/nvim/lua/config/options.lua
+
 ln -vf autocmds.lua ~/.config/nvim/lua/config/autocmds.lua
 ln -vf keymaps.lua ~/.config/nvim/lua/config/keymaps.lua
 ln -vf options.lua ~/.config/nvim/lua/config/options.lua

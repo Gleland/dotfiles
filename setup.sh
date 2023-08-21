165db02a3f238bed805be84b49834b5b23a77d00
Garrett Neugent <garrett.neugent@gopuff.com>
2023-08-21 11:36:01 -0600

    reorganize zshrc

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 https://taskfile.dev/installation/
 
 echo "===== Installing Task to manage the dotfiles environment. ====="
-sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b /usr/local/bin/
+sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b ~/.local/bin
 
 echo "===== Task installed. Running task --list for a preview of what to install in this environment. ====="
 
